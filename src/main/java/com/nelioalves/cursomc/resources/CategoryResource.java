0e58469bfa9292a2399017ff42e3815d21e87668
Davi <davimargelo@hotmail.com>
2020-01-11 13:59:33 -0300

    First domain Class: Category

--- src/main/java/com/nelioalves/cursomc/resources/CategoryResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.nelioalves.cursomc.resources;
 
+import com.nelioalves.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @GetMapping
-    public String list () {
-        return "REST works!";
+    public List<Category> list() {
+
+        Category cat1 = new Category(1, "TI");
+        Category cat2 = new Category(2, "Office");
+
+        List<Category> list = new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 }
