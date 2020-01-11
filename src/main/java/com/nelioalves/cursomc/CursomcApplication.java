2bceafc692b27f2a25e61bc8637a721b911fdf82
Davi <davimargelo@hotmail.com>
2020-01-11 13:44:03 -0300

    Tested REST

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,11 +1,10 @@
-package com.nelioalves.cursomc.cursomc;
+package com.nelioalves.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
