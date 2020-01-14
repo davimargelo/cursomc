b684378042ce525c2657e87ed7352796fda39a85
Davi <davimargelo@hotmail.com>
2020-01-14 20:01:34 -0300

    Command line to save into Database

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.nelioalves.cursomc;
 
+import com.nelioalves.cursomc.domain.Category;
+import com.nelioalves.cursomc.respositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "IT");
+		Category cat2 = new Category(null, "Office");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
